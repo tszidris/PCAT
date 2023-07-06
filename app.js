b19d0851aaf804dbb150b2800b2d01872e2d04d7
tszidris <idrisyyildirim@gmail.com>
2023-07-06 20:56:01 +0300

    commit3

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-const fn = function () {
-  console.log('dsdfsdfs');
-};
-fn();
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı...`);
+});
